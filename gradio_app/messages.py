f12d5d9b3fbc401fd0dec3ef4f4fa8b53ec39789
Vojtech Hudecek <vhudecek@omilia.com>
2025-05-29 12:57:30 +0200

    added messages.py

---
+++ gradio_app/messages.py
@@ -0,0 +1,71 @@
+from typing import *
+from pydantic import BaseModel
+
+
+class RequestQueryLLM(BaseModel):
+    history: List[List[str]]
+    input_text: str
+    llm_name: str
+    task_config: Optional[str] = "Basic"
+    docs_k: Optional[int]
+    temp: Optional[float]
+    top_p: Optional[float]
+    max_tokens: Optional[int]
+    index_name: Optional[str]
+    retriever_address: Optional[str] = "public"
+    system_prompt: Optional[str] = None
+
+
+class RequestBatchQuery(BaseModel):
+    llm_name: str
+    task_config: Optional[str] = "Basic"
+    docs_k: Optional[int]
+    temp: Optional[float]
+    top_p: Optional[float]
+    max_tokens: Optional[int]
+    index_name: Optional[str]
+    retriever_address: Optional[str] = "public"
+    system_prompt: Optional[str] = None
+
+
+class RequestIngest(BaseModel):
+    index_name: str
+    embed_name: str
+    chunk_size: Optional[int] = 500
+    percentile: Optional[float] = 0.9
+    splitting_strategy: Optional[str] =  "recursive"
+    retriever_address: Optional[str] = "public"
+
+
+class RequestListVS(BaseModel):
+    retriever_address: str = "public"
+
+
+class RequestFeedback(BaseModel):
+    filter_column: Optional[str] = None
+    filter_value: Optional[str] = None
+
+
+class ResponseQueryLLM(BaseModel):
+    text: str
+    documents: List[str]
+    error: Optional[str] = None
+
+
+class ResponseBatchQuery(BaseModel):
+    processed: Dict
+    error: Optional[str] = None
+
+
+class ResponseIngest(BaseModel):
+    status: str
+    msg: str
+
+
+class ResponseList(BaseModel):
+    available: List[str]
+
+
+class ResponseFeedback(BaseModel):
+    filter: str
+    feedback: List[Dict]
