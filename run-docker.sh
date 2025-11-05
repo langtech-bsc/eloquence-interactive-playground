9662de8db13e6101b5bcda9fc688238b1aa88874
ELOQUENCE-CI <eloquence-ci@eloquence.ai>
2025-11-05 09:16:32 +0000

    Update run-docker.sh

--- run-docker.sh
+++ run-docker.sh
@@ -1,2 +1,3 @@
 docker build -t eloquence-ip:prod .
+docker rm -f eloquence-ip-prod 2>/dev/null || true
 docker run -d --name eloquence-ip-prod --net="host" -v `pwd`/playground-data:/data eloquence-ip:prod
