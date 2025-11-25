ae854493c15066182795bef9775bd4b39cd0417f
ELOQUENCE-CI <eloquence-ci@eloquence.ai>
2025-11-25 17:34:22 +0000

    now it works

--- run-docker.sh
+++ run-docker.sh
@@ -1,3 +1,6 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
 docker build -t eloquence-ip:prod .
 docker rm -f eloquence-ip-prod 2>/dev/null || true
 docker run -d --name eloquence-ip-prod --net="host" -v `pwd`/playground-data:/data eloquence-ip:prod
